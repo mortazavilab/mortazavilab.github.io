0cc4140e88879894f3c111e11c1a9a513bf0fed7
Narges Rezaie <57965133+nargesr@users.noreply.github.com>
2020-08-24 22:33:48 -0700

    Update convertor.py

--- _bibliography/convertor.py
+++ _bibliography/convertor.py
@@ -1,39 +1,52 @@
 import numpy as np
+import pandas as pd
 
 bibtex = open("papers.bib", "r")
 file = bibtex.read()
 
 papers = file.split("@")[1:]
-resluts = np.zeros((len(papers), 4), dtype=object)
+title = []
+author = []
+journal = []
+year = []
 
 for i in range(len(papers)):
     papers[i] = papers[i].replace(":", ";")
     tmp = papers[i].split("\n")
     for t in tmp:
-        if "title=" in t:
-            resluts[i, 0] = t[9:-2]
+        if "title=" in t and "booktitle" not in t:
+            title.append(t[9:-2])
         if "author=" in t:
-            resluts[i, 1] = t[10:-2]
+            author.append(t[10:-2])
         if "journal=" in t:
-            resluts[i, 2] = t[11:-2]
+            journal.append(t[11:-2])
         if "year=" in t:
             if "," in t:
-                resluts[i, 3] = t[8:-2]
+                year.append(int(t[8:-2]))
             else:
-                resluts[i, 3] = t[8:-1]
+                year.append(int(t[8:-1]))
 
-resluts = resluts[resluts[:, 3][::-1].sort()][0]
+    if len(title) != len(author):
+        author.append("")
+
+    if len(title) != len(journal):
+        journal.append("")
+
+    if len(title) != len(year):
+        year.append(0)
+
+data = {'name': title, 'author': author, 'journal': journal, 'year': year}
+results = pd.DataFrame(data)
+
+results = results.sort_values(by=['year'], ascending=False).values
 
 res = ""
 
-for result in resluts:
-    for i in range(len(result)):
-        if result[i] == 0:
-            result[i] = ""
-    res = res + "      - title: " + result[0]\
-          + "\n        author: " + result[1]\
-          + "\n        journal: " + result[2]\
-          + "\n        year: " + str(result[3]) + "\n\n"
+for i in range(len(results)):
+    res = res + "      - title: " + results[i, 0]\
+          + "\n        author: " + results[i, 1]\
+          + "\n        journal: " + results[i, 2]\
+          + "\n        year: " + str(results[i, 3]) + "\n\n"
 
 file = open('text', 'w')
 file.write(res)
